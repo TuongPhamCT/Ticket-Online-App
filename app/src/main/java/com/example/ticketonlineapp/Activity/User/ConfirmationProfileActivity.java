87bf662d0e718d9977eb870c5d41b63fa9f20be0
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-03-10 16:28:45 +0700

    TOA-44 build checking network feature

--- app/src/main/java/com/example/ticketonlineapp/Activity/User/ConfirmationProfileActivity.java
+++ app/src/main/java/com/example/ticketonlineapp/Activity/User/ConfirmationProfileActivity.java
@@ -2,17 +2,32 @@ package com.example.ticketonlineapp.Activity.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.ticketonlineapp.Activity.Network.CheckNetwork;
 import com.example.ticketonlineapp.R;
 import com.makeramen.roundedimageview.RoundedImageView;
 
 public class ConfirmationProfileActivity extends AppCompatActivity {
+    CheckNetwork checkNetwork = new CheckNetwork();
+    @Override
+    protected void onStart() {
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(checkNetwork, filter);
+        super.onStart();
+    }
 
+    @Override
+    protected void onStop() {
+        unregisterReceiver(checkNetwork);
+        super.onStop();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/ticketonlineapp/Activity/User/OnboardingActivity.java
+++ app/src/main/java/com/example/ticketonlineapp/Activity/User/OnboardingActivity.java
@@ -2,15 +2,31 @@ package com.example.ticketonlineapp.Activity.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.ticketonlineapp.Activity.Network.CheckNetwork;
 import com.example.ticketonlineapp.R;
 
 public class OnboardingActivity extends AppCompatActivity {
 
+    CheckNetwork checkNetwork = new CheckNetwork();
+    @Override
+    protected void onStart() {
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(checkNetwork, filter);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        unregisterReceiver(checkNetwork);
+        super.onStop();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
