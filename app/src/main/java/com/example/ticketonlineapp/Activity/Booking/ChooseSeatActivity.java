01994a3b784dfaac52082ec746bda61d5508652d
VuDucTruong <21520507@gm.uit.edu.vn>
2024-03-12 22:30:38 +0700

    create UI choose seat activity

---
+++ app/src/main/java/com/example/ticketonlineapp/Activity/Booking/ChooseSeatActivity.java
@@ -0,0 +1,16 @@
+package com.example.ticketonlineapp.Activity.Booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ticketonlineapp.R;
+
+public class ChooseSeatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_seat);
+    }
+}
