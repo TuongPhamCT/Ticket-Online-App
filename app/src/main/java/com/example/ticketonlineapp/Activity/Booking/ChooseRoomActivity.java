1060aebe8ee042e65e4230f03f2fce2e36351722
HuynhDangQuang <quangptnk@gmail.com>
2024-03-19 21:29:24 +0700

    Finished design Choose room

---
+++ app/src/main/java/com/example/ticketonlineapp/Activity/Booking/ChooseRoomActivity.java
@@ -0,0 +1,17 @@
+package com.example.ticketonlineapp.Activity.Booking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ticketonlineapp.R;
+
+public class ChooseRoomActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_room);
+    }
+
+}
