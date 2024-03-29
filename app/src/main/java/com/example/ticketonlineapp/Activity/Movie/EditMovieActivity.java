a1c9e8643883c895be34d5c25a11e9ac8d3b6ad2
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-03-29 22:36:33 +0700

    TOA-21 Done

---
+++ app/src/main/java/com/example/ticketonlineapp/Activity/Movie/EditMovieActivity.java
@@ -0,0 +1,16 @@
+package com.example.ticketonlineapp.Activity.Movie;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ticketonlineapp.R;
+
+public class EditMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_movie_screen);
+    }
+}
