c90bc4850dc37f6aac73754eb9e76f0540222200
Phạm Thanh Tường <tuongpt12503@gmail.com>
2024-03-31 18:46:35 +0700

    TOA-52 full code notification detail

--- app/src/main/java/com/example/ticketonlineapp/Activity/Notification/NotificationDetailActivity.java
+++ app/src/main/java/com/example/ticketonlineapp/Activity/Notification/NotificationDetailActivity.java
@@ -1,16 +1,107 @@
 package com.example.ticketonlineapp.Activity.Notification;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ticketonlineapp.Activity.HomeActivity;
+import com.example.ticketonlineapp.Activity.Network.CheckNetwork;
+import com.example.ticketonlineapp.Activity.Report.ReportActivity;
+import com.example.ticketonlineapp.Activity.Ticket.MyTicketActivity;
+import com.example.ticketonlineapp.Activity.Wallet.MyWalletActivity;
+import com.example.ticketonlineapp.Model.ExtraIntent;
+import com.example.ticketonlineapp.Model.Notification;
+import com.example.ticketonlineapp.Model.Users;
 import com.example.ticketonlineapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NotificationDetailActivity extends AppCompatActivity {
+    CheckNetwork checkNetwork = new CheckNetwork();
+    @Override
+    protected void onStart() {
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(checkNetwork, filter);
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+        unregisterReceiver(checkNetwork);
+        super.onStop();
+    }
+
+    ImageView backBtn;
+    private BottomNavigationView bottomNavigationView;
+    TextView title;
+    TextView description;
+    Notification notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification_detail_screen);
+        BottomNavigation();
+        backBtn = findViewById(R.id.btnBack);
+        title = findViewById(R.id.titleContent);
+        description = findViewById(R.id.descriptionContent);
+        Intent intent = getIntent();
+        notification = intent.getParcelableExtra(ExtraIntent.notification);
+
+        if(notification!=null)
+        {
+            title.setText(notification.getHeading());
+            description.setText(notification.getDescription());
+        }
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+    void BottomNavigation()
+    {
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        bottomNavigationView.getMenu().findItem(R.id.NotificationPage).setChecked(true);
+        try{if (Users.currentUser != null)
+            if (((Users.currentUser.getAccountType().toString()).equals("admin"))) {
+                Menu menu = bottomNavigationView.getMenu();
+                MenuItem ReportPage = menu.findItem(R.id.ReportPage);
+                MenuItem WalletPage = menu.findItem(R.id.walletPage);
+                WalletPage.setVisible(false);
+                ReportPage.setVisible(true);
+            }
+        }
+        catch (Exception e){}
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.homePage:
+                    startActivity(new Intent(NotificationDetailActivity.this, HomeActivity.class));
+                    overridePendingTransition(0,0);
+                    break;
+                case R.id.ticketPage:
+                    startActivity(new Intent(NotificationDetailActivity.this, MyTicketActivity.class));
+                    overridePendingTransition(0,0);
+                    break;
+                case R.id.walletPage:
+                    startActivity(new Intent(NotificationDetailActivity.this, MyWalletActivity.class));
+                    overridePendingTransition(0,0);
+                    break;
+                case R.id.ReportPage:
+                    startActivity(new Intent(NotificationDetailActivity.this, ReportActivity.class));
+                    overridePendingTransition(0,0);
+                    break;
+            }
+            return true;
+        });
     }
 }
